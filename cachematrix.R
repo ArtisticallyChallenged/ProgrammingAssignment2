d0311244e137acd34193b4eae21b2176f2b4be6d
ArtisticallyChallenged <karen.hunt@ninosalvaggio.com>
2015-09-27 14:05:31 -0400

    Instead of Checking for a null inverse, check for the same matrix

--- cachematrix.R
+++ cachematrix.R
@@ -10,6 +10,8 @@
 makeCacheMatrix <- function(x = matrix()) {  
      m <- NULL
      n <- NULL
+     setmatrix <-function(n) n <<- x
+     getmatrix <- function() n
      set <- function(y) {
      x <<- y
      m <<- NULL
@@ -17,8 +19,10 @@ makeCacheMatrix <- function(x = matrix()) {
      get <- function() x
      setsolve <- function(solve) m <<- solve
      getsolve <- function() m
-     setmatrix <-function(z) n <<-x
-     list(set = set, get = get,setmatrix=setmatrix
+     
+     list(setmatrix = setmatrix,
+          getmatrix = getmatrix,
+          set = set, get = get,
           setsolve = setsolve,
           getsolve = getsolve)
 }
@@ -26,13 +30,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
+## Checks to see if new matrix is the same as cache matrix
+## If so, returns the inverse.
+## If not, calculates the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-     m <- x$getsolve()
-     n <- x$setmatrix()
-     if(!is.null(m) && !is.null(n)) {
+          m <- x$getsolve()
+        ## If the new matrix is the same as the cache matrix
+        ## Returns the cache inverse     
+     if(x == n) {
           message("getting cached data")
           return(m)
      }
